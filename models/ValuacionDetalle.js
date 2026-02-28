84103cc1ec98137631fe39aab2f931b63cb16b62
luzpardina <lpardina@goodies.com.ar>
2026-02-28 12:25:00 -0300

    Update ValuacionDetalle.js

--- models/ValuacionDetalle.js
+++ models/ValuacionDetalle.js
@@ -48,6 +48,35 @@ const ValuacionDetalle = sequelize.define('ValuacionDetalle', {
     diferencia_pct: {
         type: DataTypes.DECIMAL(10, 2),
         defaultValue: 0
+    },
+    // Nuevos campos
+    estado: {
+        type: DataTypes.STRING(50),
+        defaultValue: 'OK'
+    },
+    nombre_costeo_origen: {
+        type: DataTypes.STRING(300),
+        defaultValue: ''
+    },
+    fecha_despacho_origen: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    nro_despacho_origen: {
+        type: DataTypes.STRING(100),
+        defaultValue: ''
+    },
+    proveedor_origen: {
+        type: DataTypes.STRING(300),
+        defaultValue: ''
+    },
+    codigo_sistema: {
+        type: DataTypes.STRING(100),
+        defaultValue: ''
+    },
+    nombre_sistema: {
+        type: DataTypes.STRING(500),
+        defaultValue: ''
     }
 }, {
     tableName: 'valuacion_detalle',
